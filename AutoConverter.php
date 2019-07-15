3be3b1421235b8e4c78a8f2447e8cfeebba91e9f
Ashutosh <ashutosh.sharma@vaidam.com>
2019-07-15 17:01:57 +0530

    convert new files

--- AutoConverter.php
+++ AutoConverter.php
@@ -15,21 +15,38 @@ class AutoConverter  extends FetchDirectory
     private $outputdir;
     private $quality=75;
     private $image_directory_array=[];
+
     const IMAGE_QUALITIES = ['25', '50', '75', '100'];
 
-    public function __construct($inputdir, $outputdir) {
+    private $convert_new_file_status = true;
+
+    public function __construct($inputdir, $outputdir, $convert_new_file_status=true) {
         $this->setImageQuality();
-        $this->inputdir = $inputdir;
-        $this->outputdir = $outputdir;
-        $this->image_directory_array = $this->subDirectoriesArray($this->inputdir);
+        $this->inputdir                 = $inputdir;
+        $this->outputdir                = $outputdir;
+        $this->convert_new_file_status         = $convert_new_file_status;
+        $this->image_directory_array    = $this->subDirectoriesArray($this->inputdir);
+    }
+
+    private function getNewFiles(array $allImageArray)
+    {
+        if ($this->convert_new_file_status) {
+            $newFilesArray = array_filter($allImageArray, function ($file) {
+                return filemtime($file) >= time() - 5*60;
+            });
+            return $newFilesArray;
+        }
+        return $allImageArray;
     }
 
     public function convert()
     {
         foreach ($this->image_directory_array as $dir) {
-            $imageAr= $this->getImageArrayFromDirectory($dir);
+            $imageArray     = $this->getImageArrayFromDirectory($dir);
+            $newFilesArray  = $this->getNewFiles($imageArray);
+            
             try {
-                foreach ($imageAr as $value) {
+                foreach ($newFilesArray as $value) {
                     //encode images to webp
                     $image = Image::make($value)->encode('webp');
                     //Filename without Extension
@@ -38,13 +55,13 @@ class AutoConverter  extends FetchDirectory
                     $filenameToStore= $filename.'.webp';
 
                     foreach (self::IMAGE_QUALITIES as $image_quality) {
-                        $this->quality = (int)$image_quality;
-                        $path_to_save="$this->outputdir$image_quality/$dir";
+                        $this->quality  = (int)$image_quality;
+                        $path_to_save   = "$this->outputdir$image_quality/$dir";
                         
                         if (!file_exists($path_to_save)) {
                             mkdir($path_to_save, 666, true);
                         }
-                        
+
                         $image->save("$this->outputdir$image_quality/$dir$filenameToStore", $this->quality);
                     }
                 }
@@ -74,10 +91,14 @@ class AutoConverter  extends FetchDirectory
 
 /**
  * Don't append forward slash to $image_directory Array.
+ * Parameters of class instantiation
+ * First    => Image Directory(without forward slash)
+ * Second   => Ouptput Directory
+ * Third    => Default(true) for new image converting only
  */
-$image_directory= 'pictures/all';  $output_directory= 'pictures/converted/';
+$image_directory= 'pictures/files';  $output_directory= 'pictures/webp/';
 
-$convert= new AutoConverter($image_directory, $output_directory);
+$convert= new AutoConverter($image_directory, $output_directory, false);
 $convert->convert();
 
 /**
@@ -86,7 +107,7 @@ $convert->convert();
  * create directory if not found on output directory--------
  * Save files in each directory-----------------------------
  * 25,50,75,100---------------------------------------------
- * Check for new files
- * convert only them
+ * Check for new files--------------------------------------
+ * convert only them----------------------------------------
  */
 
