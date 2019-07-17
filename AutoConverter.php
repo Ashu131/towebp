41d265f6b053678ff31185f5c4286018d11a1f95
Ashutosh <ashutosh.sharma@vaidam.com>
2019-07-17 12:56:56 +0530

    FetchDirectory function moved to AutoConverter file, Image directory full path

--- AutoConverter.php
+++ AutoConverter.php
@@ -1,16 +1,14 @@
 <?php
-// namespace App;
 
 require 'vendor/autoload.php';
 use Intervention\Image\ImageManagerStatic as Image;
-// use Directory;
-require_once 'FetchDirectory.php';
 
-class AutoConverter  extends FetchDirectory
+class AutoConverter 
 {
     /**
      * Pass second parameter for quality ex. -q75
      */
+    private $rootdir;
     private $inputdir;
     private $outputdir;
     private $quality=75;
@@ -20,11 +18,12 @@ class AutoConverter  extends FetchDirectory
 
     private $convert_new_file_status = true;
 
-    public function __construct($inputdir, $outputdir, $convert_new_file_status=true) {
+    public function __construct($rootdir, $inputdir, $outputdir, $convert_new_file_status=true) {
         $this->setImageQuality();
+        $this->rootdir                 = $rootdir;
         $this->inputdir                 = $inputdir;
         $this->outputdir                = $outputdir;
-        $this->convert_new_file_status         = $convert_new_file_status;
+        $this->convert_new_file_status  = $convert_new_file_status;
         $this->image_directory_array    = $this->subDirectoriesArray($this->inputdir);
     }
 
@@ -32,7 +31,7 @@ class AutoConverter  extends FetchDirectory
     {
         if ($this->convert_new_file_status) {
             $newFilesArray = array_filter($allImageArray, function ($file) {
-                return filemtime($file) >= time() - 5*60;
+                return filemtime($file) >= strtotime("-1 days");
             });
             return $newFilesArray;
         }
@@ -44,6 +43,7 @@ class AutoConverter  extends FetchDirectory
         foreach ($this->image_directory_array as $dir) {
             $imageArray     = $this->getImageArrayFromDirectory($dir);
             $newFilesArray  = $this->getNewFiles($imageArray);
+            $img_save_directory = str_replace($this->rootdir,'',$dir);
             
             try {
                 foreach ($newFilesArray as $value) {
@@ -56,13 +56,13 @@ class AutoConverter  extends FetchDirectory
 
                     foreach (self::IMAGE_QUALITIES as $image_quality) {
                         $this->quality  = (int)$image_quality;
-                        $path_to_save   = "$this->outputdir$image_quality/$dir";
+                        $path_to_save   = "$this->outputdir$image_quality/$img_save_directory";
                         
                         if (!file_exists($path_to_save)) {
                             mkdir($path_to_save, 666, true);
                         }
 
-                        $image->save("$this->outputdir$image_quality/$dir$filenameToStore", $this->quality);
+                        $image->save("$this->outputdir$image_quality/$img_save_directory$filenameToStore", $this->quality);
                     }
                 }
             } catch (\Exception $e) {
@@ -87,6 +87,33 @@ class AutoConverter  extends FetchDirectory
         }
         return $images;
     }
+
+    /**
+     * This function will alter array values
+     * by "concate" function
+     */
+    public function subDirectoriesArray($dir= 'pictures/all')
+    {
+        $subDirArr = $this->getSubDirectories($dir);
+        array_walk($subDirArr,array($this,"concate"));
+        return $subDirArr;
+    }
+
+    private function concate(&$value,$key)
+    {
+        $value.="/";
+    }
+
+    public function getSubDirectories($dir)
+    {
+        $subDir = array();
+        $directories = array_filter(glob($dir), 'is_dir');
+        $subDir = array_merge($subDir, $directories);
+        foreach ($directories as $directory){
+            $subDir = array_merge($subDir, $this->getSubDirectories($directory.'/*'));
+        }
+        return $subDir;
+    }
 }
 
 /**
@@ -96,18 +123,8 @@ class AutoConverter  extends FetchDirectory
  * Second   => Ouptput Directory
  * Third    => Default(true) for new image converting only
  */
-$image_directory= 'pictures/files';  $output_directory= 'pictures/webp/';
+$root_directory = '/var/www/html/ashutosh/modules/imagetowebp/pictures';
+$image_directory= $root_directory.'/all';  $output_directory= $root_directory.'/new/';
 
-$convert= new AutoConverter($image_directory, $output_directory, false);
+$convert= new AutoConverter($root_directory, $image_directory, $output_directory, false);
 $convert->convert();
-
-/**
- * Task-------
- * Loop through input directories---------------------------
- * create directory if not found on output directory--------
- * Save files in each directory-----------------------------
- * 25,50,75,100---------------------------------------------
- * Check for new files--------------------------------------
- * convert only them----------------------------------------
- */
-
